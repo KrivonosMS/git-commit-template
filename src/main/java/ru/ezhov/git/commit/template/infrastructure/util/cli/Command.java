9e581caec5de6c252325ca3c923d40aebd136e86
ezhov-da <ezhdenis@yandex.ru>
2019-09-08 10:49:43 +0300

    refactor(infrastructure): imports fixed

--- src/main/java/ru/ezhov/git/commit/template/infrastructure/util/cli/Command.java
+++ src/main/java/ru/ezhov/git/commit/template/infrastructure/util/cli/Command.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 public class Command {
     private final File workingDirectory;
