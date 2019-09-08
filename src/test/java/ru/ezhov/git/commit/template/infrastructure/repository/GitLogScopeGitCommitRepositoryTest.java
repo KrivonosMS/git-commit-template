ae4cf7d71596984a5cc8c577409dd92b86c36422
ezhov-da <ezhdenis@yandex.ru>
2019-09-08 10:05:04 +0300

    fix(infrastructure): откорректировал обработку scope

--- src/test/java/ru/ezhov/git/commit/template/infrastructure/repository/GitLogScopeGitCommitRepositoryTest.java
+++ src/test/java/ru/ezhov/git/commit/template/infrastructure/repository/GitLogScopeGitCommitRepositoryTest.java
@@ -1,8 +1,10 @@
 package ru.ezhov.git.commit.template.infrastructure.repository;
 
 import org.junit.Test;
+import ru.ezhov.git.commit.template.model.domain.ScopeOfChange;
 
 import java.io.File;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -10,6 +12,9 @@ public class GitLogScopeGitCommitRepositoryTest {
     @Test
     public void scopesOfChange() {
         GitLogScopeGitCommitRepository gitLogScopeGitCommitRepository = new GitLogScopeGitCommitRepository(new File("."));
-        gitLogScopeGitCommitRepository.scopesOfChange();
+
+        List<ScopeOfChange> scopeOfChanges = gitLogScopeGitCommitRepository.scopesOfChange();
+
+        scopeOfChanges.forEach(System.out::println);
     }
 }
